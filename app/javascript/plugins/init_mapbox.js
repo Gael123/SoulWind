d6ca24262f083647f457177a5081cde9b7c74de9
Gael Jerop <gael.jerop@yahoo.com>
2020-03-24 15:19:38 +1100

    fixed map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,17 +1,37 @@
 import mapboxgl from 'mapbox-gl';
-
+// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const mapElement = document.getElementById('map');
-
 
 const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-     center: [151.21535, -34.00955], // starting position
-    zoom: 12 // starting zoom
+    duration: 0.8,
+    center: [151.2069,-34.0116],
+    zoom: 14,
+
+
   });
 }
+
+// const geocoder = new MapboxGeocoder({
+// accessToken: mapboxgl.accessToken,
+// mapboxgl: mapboxgl
+// });
+
+const addControl = (map) => {
+  map.addControl(
+new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+})
+);
+}
+
+
 
 const addMarkersTo = (map, markers) => {
 
@@ -29,26 +49,30 @@ const addMarkersTo = (map, markers) => {
     new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
+
   });
 }
 
-const fitMapToMarkers = (map, markers) => {
+  const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
+ map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
 };
 
 const initMapbox = () => {
   if (mapElement) {
     // Build map
     const map = buildMap();
-
+    const position = addControl(map);
     // Add Markers
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersTo(map, markers);
 
     // Fit to markers
     fitMapToMarkers(map, markers);
+     addControl(map);
+
   }
+
 };
 export { initMapbox };
